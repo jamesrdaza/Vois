d959e31d5c595e1587e902e143797c8bf65ee9bf
heyitsmeposi <heyitsmeposi@gmail.com>
2022-08-10 10:34:10 -0700

    Testing Packages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import { web3 } from "web3";
 
 export default function home() {
   return (
 
     <div className={styles.container}>
       <Head>
-        <title>App Test</title>
-        <meta name="description" content="AppTest" />
+        <title>Vois</title>
+        <meta name="description" content="Vois" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -17,7 +18,7 @@ export default function home() {
           AppTest
         </h1>
         <div className={styles.buttonContainer} >
-          <Link href="/signup" className={styles.button}>Create Account</Link>
+          <Link href="/signup" className={styles.button}></Link>
           <Link href="/login">Sign In</Link>
         </div>
 
